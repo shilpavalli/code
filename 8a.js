2a8e3a5e327820e3d3030dfabd7c844407b50cb7
shilpavalli <88371549+shilpavalli@users.noreply.github.com>
2021-08-03 19:36:16 +0530

    {firstName: "Philip", lastName: "Fry",
    {firstName: "Philip", lastName: "Fry", job: "Delivery Boy", employer: "Planet Express"}

--- 8a.js
+++ 8a.js
@@ -1,1 +1,14 @@
+const name = {
+    firstName: 'Philip',
+    lastName: 'Fry'
+};
 
+const details = {
+    job: 'Delivery Boy',
+    employer: 'Planet Express'
+};
+
+// Merge the objects
+const character = Object.assign(name, details);
+
+console.log(character);
